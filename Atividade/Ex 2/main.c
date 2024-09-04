37c57dfa7af79befc9af8d414e5df09906a3bf1f
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-09-04 18:36:41 -0300

    Update main.c

--- Atividade/Ex 2/main.c
+++ Atividade/Ex 2/main.c
@@ -3,11 +3,12 @@
 int main(){
     No* lista = NULL;
 
+    lista = inserir_no_inicio(lista, 8);
     lista = inserir_no_inicio(lista, 5);
     lista = inserir_no_inicio(lista, 2);
     lista = inserir_no_fim(lista, 3);
 
-    printf("Lista inicial:\n");
+    printf("\n");
     exibir_lista(lista);
 
     lista = inserir_apos(lista, 2, 6);
@@ -23,6 +24,7 @@ int main(){
     exibir_lista(lista);
 
     No *num = remover_primeiro(lista);
+    
     printf("\n");
     exibir_lista(lista);
 
